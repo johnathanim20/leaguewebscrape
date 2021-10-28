82e8817b827201bebb14cc8f26a8085211196a89
unknown <jim20@illinois.edu>
2021-10-28 16:26:41 -0500

    scraping and database implemented

---
+++ src/api.py
@@ -0,0 +1,6 @@
+import datetime
+import requests
+import json
+import pymongo
+import argparse
+from flask import Flask, request, jsonify, render_template
---
+++ src/database.py
@@ -0,0 +1,40 @@
+import pymongo
+import os
+from dotenv import load_dotenv
+
+def get_key():
+    """
+    This function gets the unique key to access the MongoDB database collection
+    """
+    load_dotenv('C:\\Users\\johna\\fa21-cs242-project\\src\\key.env')
+    return os.getenv("SECRET_KEY")
+
+def database_handler(ret_arr1):
+    """
+    This function handles the web scraped data of a book and transfers it into the MongoDB database.
+    """
+    if ret_arr1 is not None:
+        client = pymongo.MongoClient(get_key())
+        database = client.Collection
+        collection = database.Champions
+        champ = {"name": ret_arr1[0],
+                "win_rate" : ret_arr1[1],
+                "pick_rate" : ret_arr1[2],
+                "champ_tier" : ret_arr1[3],
+                "strong_against" : ret_arr1[4],
+                "counter_champs" : ret_arr1[5],
+                }
+        collection.update(champ, champ, upsert = True)
+        
+def main():
+    arr = []
+    arr.append("Aatrox")
+    arr.append("12%")
+    arr.append("56%")
+    arr.append("hi")
+    arr.append("bye")
+    arr.append("yo")
+    database_handler(arr)
+
+if __name__ == "__main__":
+    main()
